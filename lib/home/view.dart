f497242f78b1074a7389616345e50f8ab409cf92
Nickolas de Luca Alberton <nickolasdeluca@live.com>
2023-06-16 13:45:34 -0300

    Fix translation

--- lib/home/view.dart
+++ lib/home/view.dart
@@ -23,7 +23,7 @@ class _HomeState extends State<Home> {
               builder: (context) => const CameraWidget(),
             ),
           ),
-          child: const Text('Abrir câmera'),
+          child: const Text('Open camera'),
         ),
       ),
     );
